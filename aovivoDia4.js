8c3d9bc84fdb7ff7c35f27cae36a180ce2c86465
Gabriel Mouro <gabrielmouro38@gmail.com>
2022-11-30 17:42:05 -0300

    Exemplo function

---
+++ aovivoDia4.js
@@ -0,0 +1,11 @@
+function bigger (num1, num2) {
+    if (num1 > num2) {
+       return 'O maior é o: ' + num1;
+    } else if (num1 === num2) {
+        return 'Empate: ' + num1;
+    } else {
+        return 'O maior é o: ' + num2;
+    }
+}
+
+console.log(bigger(9, 9));
