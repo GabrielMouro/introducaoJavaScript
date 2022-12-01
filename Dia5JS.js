8b008d69ab3219b48085ee78afae47f350026e11
Gabriel Mouro <gabrielmouro38@gmail.com>
2022-12-01 16:23:19 -0300

    inicio ao vivo dia5 secao 4

---
+++ Dia5JS.js
@@ -0,0 +1,20 @@
+const printName = () => 'Lucas';
+console.log(printName());
+
+console.log(`
+-----------------fim do primero exemplo-----------------
+`);
+
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+console.log(`
+------------------fim do segundo exemplo----------------
+`);
+
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(85669, 2));
+
+console.log(`
+---------------------fim do terceiro exemplo)------------
+`);
